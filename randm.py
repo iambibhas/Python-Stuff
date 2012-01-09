84a9125bfc6ce291dba487bf8c8c2cde80a4da7b
Bibhas C Debnath <iambibhas@gmail.com>
2012-01-09 22:09:26 +0530

    Random string generator added

---
+++ randm.py
@@ -0,0 +1,39 @@
+import sys
+import random
+
+count = int(sys.argv[1])
+randm = {}
+def get_random_string(count):
+    string = ''
+    avail_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?><._abcdefghijklmnopqrstuvwxyz1234567890'
+
+    for i in range(0, count):
+        randm[0] = random.randrange(0, len(avail_chars))
+        randm[1] = random.randrange(0, len(avail_chars))
+        rand = random.randrange(0,2)
+        string += avail_chars[randm[rand]]
+
+    return string
+
+def levenshtein(a,b):
+    "Calculates the Levenshtein distance between a and b."
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n,m)) space
+        a,b = b,a
+        n,m = m,n
+
+    current = range(n+1)
+    for i in range(1,m+1):
+        previous, current = current, [i]+[0]*n
+        for j in range(1,n+1):
+            add, delete = previous[j]+1, current[j-1]+1
+            change = previous[j-1]
+            if a[j-1] != b[i-1]:
+                change = change + 1
+            current[j] = min(add, delete, change)
+
+    return current[n]
+
+random_string = get_random_string(count)
+print random_string
